0f060a1102fd39dace2de4fd8d93634f0da8f247
szt2palpat <szt2palpat@vasvari.org>
2023-09-14 13:17:38 +0200

    Codinghours

---
+++ src/CodingHours.java
@@ -0,0 +1,9 @@
+public class CodingHours {
+    public static void main(String[] args) {
+        int code=6*5*17;
+
+        System.out.println(code);
+        double codePc=(code/(52f*17f))+100;
+        System.out.println(codePc);
+    }
+}
